a0a93058964f347692c33d32879474213d430e5b
zemek <zemek@01004296-7ca7-4d85-b7c5-24bb6f2cc6b5>
2013-11-04 01:59:50 +0000

    Init project: PrzeszukiwarkaPlikow
    git-svn-id: https://lab.iisg.agh.edu.pl/svn/to2/branches/PrzeszukiwarkaPlikow@6 01004296-7ca7-4d85-b7c5-24bb6f2cc6b5

---
+++ ParserPlikow/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserPlikow
+{
+    public class Class1
+    {
+    }
+}
