a5468fe93d4c2a464fd71989f529decefa9ac416
blostic <piotr.skibiak@gmail.com>
2013-11-08 22:47:58 +0100

    SearchSummary module interface

---
+++ FileScanner.SearchSummary/SummaryGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchSummary
+{
+    class SummaryGenerator: ISummaryGenerator
+    {
+        void Generate(IEnumerable<FileInfo> searchResults)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
