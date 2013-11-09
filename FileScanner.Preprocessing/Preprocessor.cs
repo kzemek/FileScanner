8f6b044354e562607dcbcac6c89885b31b2b3284
MichalPawlicki <mic.pawlicki@gmail.com>
2013-11-09 14:20:47 +0100

    Implemented mapping of Polish characters to English ones

--- FileScanner.Preprocessing/Preprocessor.cs
+++ FileScanner.Preprocessing/Preprocessor.cs
@@ -8,9 +8,31 @@ namespace FileScanner.Preprocessing
 {
     public class Preprocessor : IPreprocessor
     {
+        private static Dictionary<char, char> _mappings = new Dictionary<char, char>() {
+            {'ą', 'a'},
+            {'ć', 'c'},
+            {'ę', 'e'},
+            {'ł', 'l'},
+            {'ń', 'n'},
+            {'ó', 'o'},
+            {'ś', 's'},
+            {'ź', 'z'},
+            {'ż', 'z'}
+        };
+
         public String GetNormalizedPhrase(String phrase)
         {
-            return phrase;
+            char[] phraseCharacters = phrase.ToCharArray();
+            var normalizedCharacters = phraseCharacters.Select(GetNormalizedCharacter);
+            return new String(normalizedCharacters.ToArray());
+        }
+
+        private char GetNormalizedCharacter(char c)
+        {
+            if (_mappings.ContainsKey(c))
+                return _mappings[c];
+            else
+                return c;
         }
 
         public IEnumerable<String> GetVariations(String phrase)
