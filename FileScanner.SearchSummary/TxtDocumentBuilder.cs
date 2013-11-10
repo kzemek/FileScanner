41bb8f4f4c72a9db6e83bf95930d3c4025c3bf97
blostic <piotr.skibiak@gmail.com>
2013-11-10 19:17:00 +0100

    I've added TxtDocumentBuilder class.

---
+++ FileScanner.SearchSummary/TxtDocumentBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileScanner.SearchSummary
+{
+    class TxtDocumentBuilder : IDocumentBuilder
+    {
+        private StringBuilder content;
+
+        public void AddReportHeader(DateTime generationTime)
+        {
+               
+        }
+
+        public void AddSectionHeader(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddText(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddSearchResult(SearchResult result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
