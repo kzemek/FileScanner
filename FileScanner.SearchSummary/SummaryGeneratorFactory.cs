b84f524eb463a005c4fd5d1d16ff2f7088fc4fb7
blostic <piotr.skibiak@gmail.com>
2013-11-29 22:27:01 +0100

    Add SummaryGeneratorFactory.

---
+++ FileScanner.SearchSummary/SummaryGeneratorFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileScanner.SearchSummary
+{
+    /// <summary>
+    /// Used for creating ISummaryGenerator implemetations.
+    /// </summary>
+    class SummaryGeneratorFactory
+    {
+        /// <summary>
+        /// Creates a new object that implements ISummaryGenerator interface.
+        /// </summary>
+        /// <returns>A search summary generator.</returns>
+        public static ISummaryGenerator Create()
+        {
+            return new SummaryGenerator();
+        }
+    }
+}
