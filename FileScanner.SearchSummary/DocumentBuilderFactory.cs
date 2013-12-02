551075c7b5dc9116b0039699912403d858b11369
dextero <hukutizuviki@gmail.com>
2013-12-02 22:14:12 +0100

    Add DocumentBuilderFactory + tests

---
+++ FileScanner.SearchSummary/DocumentBuilderFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace FileScanner.SearchSummary
+{
+    class DocumentBuilderFactory
+    {
+        private static IDictionary<string, Type> SUPPORTED_FORMATS =
+                new ReadOnlyDictionary<string, Type>(
+                    new Dictionary<string, Type> {
+                        { "pdf", typeof(PDFDocumentBuilder) },
+                        { "txt", typeof(TxtDocumentBuilder) }
+                    });
+
+        public static IEnumerable<string> GetSupportedFormats()
+        {
+            return SUPPORTED_FORMATS.Select(keyVal => keyVal.Key);
+        }
+
+        public static IDocumentBuilder Create(string type)
+        {
+            if (!SUPPORTED_FORMATS.ContainsKey(type))
+                throw new ArgumentException("Unsupported file format: " + type);
+
+            return (IDocumentBuilder)Activator.CreateInstance(SUPPORTED_FORMATS[type]);
+        }
+    }
+}
