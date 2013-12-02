36e99545e2c2ddfb3ca7561e1fa5264487e541fe
dextero <hukutizuviki@gmail.com>
2013-12-02 22:14:12 +0100

    Add DocumentBuilderFactory + tests

---
+++ FileScanner.SearchSummary.Tests/DocumentBuilderFactoryTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace FileScanner.SearchSummary.Tests
+{
+    [TestClass]
+    public class DocumentBuilderFactoryTest
+    {
+        [TestMethod]
+        public void Create_ValidFormat()
+        {
+            IEnumerable<string> supportedFormats = DocumentBuilderFactory.GetSupportedFormats();
+
+            foreach (string format in supportedFormats)
+                Assert.IsNotNull(DocumentBuilderFactory.Create(format));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Create_InvalidFormat()
+        {
+            DocumentBuilderFactory.Create("some invalid format");
+        }
+    }
+}
