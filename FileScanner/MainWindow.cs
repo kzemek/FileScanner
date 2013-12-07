193b4cce66c9454fa29f52cb8e81a7ce859246ae
husky227 <dawid.czech@gmail.com>
2013-12-07 01:52:26 +0100

    Add Search class

--- FileScanner/MainWindow.cs
+++ FileScanner/MainWindow.cs
@@ -7,22 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using FileScanner.FileParsing;
-using FileScanner.Preprocessing;
-using FileScanner.PatternMatching;
-using System.IO;
-using FileScanner.SearchSummary;
 
 namespace FileScanner
 {
     public partial class MainWindow : Form
     {
-        private const string NoMatchesFoundMessage = "NOOOOOOOOOOOOO!!! There are no matches for your search!";
-
-        private string CurrentSearchQuery;
-        private string CurrentFilePath;
-        private IEnumerable<Match> CurrentMatches;
-
+        private Search search;
         public MainWindow()
         {
             InitializeComponent();
@@ -46,25 +36,7 @@ namespace FileScanner
             return !string.IsNullOrEmpty(searchPhraseTextBox.Text) && !string.IsNullOrEmpty(searchFileTextBox.Text);
         }
 
-        /// <summary>
-        /// Generates human-readable format of matches.
-        /// </summary>
-        /// <param name="matches"></param>
-        /// <returns>String of matches in format "Match.Index Match.Value", one match per line</returns>
-        private string BuildResults(IEnumerable<Match> matches)
-        {
-            var sb = new StringBuilder();
-
-            foreach (var m in matches)
-            {
-                sb.Append(m.Index).Append(' ').Append(m.Value).AppendLine();
-            }
-
-            return sb.ToString();
-        }
-
         #endregion
-
 
         #region Events
 
@@ -83,18 +55,13 @@ namespace FileScanner
 
         private void searchButton_Click(object sender, EventArgs e)
         {
-            var streamReader = FileParser.ParseFile(searchFileTextBox.Text,ParseMode.ReplaceCapitalLetters().ReplaceNonASCII());
-            var preprocessor = new PreprocessorFactory().GetIPreprocessor();
-            var phrases = preprocessor.GetVariations(preprocessor.GetNormalizedPhrase(searchPhraseTextBox.Text));
-            var matcher = new Matcher(phrases.ToList());
-            var matches = matcher.Matches(streamReader);
+            search = new Search(searchFileTextBox.Text, searchPhraseTextBox.Text);
+            resultsTextBox.Text = search.SearchResult();
 
-            resultsTextBox.Text = matches.Any() ? BuildResults(CurrentMatches) : NoMatchesFoundMessage;
-
-            exportResultsButton.Enabled = matches.Any();
+            exportResultsButton.Enabled = search.IsMatch();
         }
 
-
+        // TODO: czy tych dwóch funkcji nie możnaby złączyć w jedną? np. searchTextBoxes_TextChanged(...)
         private void searchPhraseTextBox_TextChanged(object sender, EventArgs e)
         {
             searchButton.Enabled = IsSearchDataProvided();
@@ -117,27 +84,7 @@ namespace FileScanner
 
         private void exportResultsButton_Click(object sender, EventArgs e)
         {
-            if (CurrentFilePath == null || CurrentSearchQuery == null || CurrentMatches == null || !CurrentMatches.Any())
-            {
-                MessageBox.Show("No results to export!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-
-            List<string> inputPaths = new List<string> { CurrentFilePath };
-            List<MatchingFile> searchResults = new List<MatchingFile>();
-            MatchingFile file;
-
-            file.fileInfo = new FileInfo(CurrentFilePath);
-            file.fileReader = FileParser.ParseFile(CurrentFilePath);
-            file.accuracy = searchResults.Count;
-            file.searchResults = CurrentMatches.GroupBy(match => match.Value)
-                                               .ToDictionary(grouping => grouping.Key,
-                                                             grouping => grouping.Select(match => match.Index));
-
-            searchResults.Add(file);
-
-            ISummaryGenerator generator = SummaryGeneratorFactory.Create();
-            generator.Generate(CurrentSearchQuery, inputPaths, searchResults);
+            search.ExportResults();
         }
 
         private void searchPhraseTextBox_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
