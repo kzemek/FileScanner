81ac78175da7ab4cbf3ce4bf08a154fc6502fda0
wojtek132 <example@example.com>
2013-12-09 01:10:39 +0100

    Update Inflector and InflectorTest

--- FileScanner.Preprocessing.Tests/InflectorTest.cs
+++ FileScanner.Preprocessing.Tests/InflectorTest.cs
@@ -20,7 +20,6 @@ namespace FileScanner.Preprocessing.Tests
             //new HashSet<string>(){"maka", "maki", "mace"}, //bez wołacza, bez liczby mnogiej
             //new HashSet<string>(){"kon", "konia", "koniowi", "koniem", "koniu"},
             //new HashSet<string>(){"oko", "oka", "oku", "okiem"},
-            //new HashSet<string>(){"monitor", "monitora", "monitorowi", "monitorem", "monitorze"},
             //new HashSet<string>(){"pies", "psa", "psu", "psem", "psie"},
             //new HashSet<string>(){"mysz", "myszy", "mysza"},
             //new HashSet<string>(){"zeglarz", "zeglarza", "zeglarzowi", "zeglarzem", "zeglarzu"},
@@ -29,12 +28,8 @@ namespace FileScanner.Preprocessing.Tests
             //new HashSet<string>(){"kot", "kota", "kotu", "kota", "kotem", "kocie"},
             //new HashSet<string>(){"ziemia", "ziemi", "ziemi", "ziemie", "ziemia", "ziemi"},
             //new HashSet<string>(){"pogoda", "pogody", "pogodzie", "pogode", "pogoda", "pogodzie"},
-            new HashSet<string>(){"droga", "drogi", "drodze", "droge", "droga", "drodze"},
-            new HashSet<string>(){"stajnia", "stajni", "stajni", "stajnie", "stajni"},//r. żeński, grupa I
-            new HashSet<string>(){"sadza", "sadzy", "sadzy", "sadzę", "sadzą", "sadzy", "sadzo"}, //r. żeński, grupa II
-            new HashSet<string>(){"kurz", "kurzu", "kurzowi", "kurz", "kurzem", "kurzu", "kurzu"},
-            new HashSet<string>(){"krol","krola","krolowi","krola","krolem","krolu","krolu"}
-            new HashSet<string>(){"grosz","grosza","groszowi","grosz","groszem","groszu","groszu"}
+
+            
         };
 
         private Inflector inflector;
@@ -62,9 +57,48 @@ namespace FileScanner.Preprocessing.Tests
                 testContextInstance = value;
             }
         }
+        [TestMethod]
+        public void TestGetVariationsMasculine()
+        {
+            TestPattern("grosz", new HashSet<string>() { "grosz", "grosza", "groszowi", "grosz", "groszem", "groszu", "groszu" }); //dziala
+            //TestPattern("kurz", new HashSet<string>(){"kurz", "kurzu", "kurzowi", "kurz", "kurzem", "kurzu", "kurzu"});
+            TestPattern("krol", new HashSet<string>(){"krol","krola","krolowi","krola","krolem","krolu","krolu"}); //dziala; uwaga, rzeczownik zywotny
+            //TestPattern("monitor", new HashSet<string>() { "monitor", "monitora", "monitorowi", "monitorem", "monitorze" });
+        }
 
-        [TestCategory("Preprocessing"), TestMethod]
-        public void TestGetVariations()
+        [TestMethod]
+        public void TestGetVariationsNeuter()
+        {
+            TestPattern("stoisko", new HashSet<string>() { "stoisko","stoiska","stoisku","stoisko","stoiskiem", "stoisku", "stoisko" });
+            
+        }
+
+        [TestMethod]
+        public void TestGetVariationsFeminine()
+        {
+            //TestPattern("droga", new HashSet<string>(){"droga", "drogi", "drodze", "droge", "droga", "drodze"});
+            //TestPattern("stajnia", new HashSet<string>(){"stajnia", "stajni", "stajni", "stajnie", "stajni"});//r. żeński, grupa I
+            //Console.Write(string.Join(" ",inflector.GetVariations("sadza grosz")));
+            TestPattern("sadza", new HashSet<string>() { "sadza", "sadzy", "sadzy", "sadzę", "sadzą", "sadzy", "sadzo" }); //r. żeński, grupa II
+           
+        }
+        public void TestPattern(string inputWord, HashSet<string> expectedWords) {
+                HashSet<string> variations = (HashSet<string>) inflector.GetVariations(inputWord);
+                Console.Write(string.Join(" ",variations));
+                Assert.IsTrue(variations.SetEquals(expectedWords));
+        }
+
+        /*[TestMethod]
+        public void TestCartesianProduct()
+        {
+            IEnumerable<String>[] array = {new List<String>(){"a","b"}, new List<String>(){"c","d"}};
+            HashSet<string> results = new HashSet<string>(){"a c", "a d", "b c", "b d"};
+            PrivateObject p = new PrivateObject(typeof(Inflector));
+            HashSet<string> variations = (HashSet<string>) p.Invoke("CartesianProduct",array, 0);
+            Assert.IsTrue(results.SetEquals(variations));
+        }*/
+        /*[TestCategory("Preprocessing"), TestMethod]
+         public void TestGetVariations()
         {
             var inputWords = getVariationsInstance;
             var expectedWords = getVariationsExpected;
@@ -77,7 +111,7 @@ namespace FileScanner.Preprocessing.Tests
                 Assert.IsTrue(variations.SetEquals(getVariationsExpected[i]));
             }
 
-        }
+        }*/
     }
 }
 
--- FileScanner.Preprocessing/Inflector.cs
+++ FileScanner.Preprocessing/Inflector.cs
@@ -7,31 +7,29 @@ namespace FileScanner.Preprocessing
 {
     public class Inflector
     {
-        /*private static String[] sufix1 = {
-            "a", "owi", "em", "ie", "y", "ow", "om","ami", "ach"
-        };*/
-
-        /*private static String[] sufixFeminine = {
-            "a", "i", "e", "e", "a" //e, ę, ą
-        };*/
 
         //class uses polish letters only internally
         private static Dictionary<string, string[]> sufixes = new Dictionary<String, String[]>() 
         {
             {"m1", new string[]{"","a","owi","","em","u","u"}},
             {"m2", new string[]{"","a","owi","a","em","u","u"}},
+            {"m3", new string[]{"","a", "a", "owi", "u", "em", "u"}}, //iem
             {"z1", new string[]{"a","i","i","ę","ą","i","o"}},
             {"z2", new string[]{"a","y","ę","o","ą"}},
             {"z3", new string[]{"a","i","e","ę","ą","e","o"}},
-            {"z4", new string[]{"a","y","e","ę","ą","e","o"}}
+            {"z4", new string[]{"a","y","e","ę","ą","e","o"}},
+            {"n2", new string[]{"o","a","u","o","em","u","o"}}
         };
 
         //order of words in dictionary is important
         private static Dictionary<string, string[]> grupyDeklinacyjneM = new Dictionary<String, String[]>() 
         {
+            
             {"m3", new string[]{"k","g","ch"}},
             {"m2", new string[]{"c","cz","dz","dż","rz","sz","ż"}},
-            {"m1", new string[]{"ć","dź","ń","ś","ź","p","b","m","w","l","j"}}
+            {"m1", new string[]{"ć","dź","ń","ś","ź","p","b","m","w","l","j"}},
+            {"m4", new string[]{"b","d","f","ł","m","n","p","r","s","t"}}
+            //grupa m5
         };
         private static Dictionary<string, string[]> grupyDeklinacyjneZ = new Dictionary<String, String[]> () 
         {
@@ -41,6 +39,11 @@ namespace FileScanner.Preprocessing
             {"z4", new string[]{"b", "d", "f", "ł", "m", "n", "p", "r", "s", "t", "w", "z"}}
         };
 
+        private static Dictionary<string, string[]> grupyDeklinacyjneN = new Dictionary<String, String[]>() 
+        {
+            //{"n1", new string[]{"ć","dź","ń","ś","ź"}},
+            {"n2", new string[]{"k","g","ch"}}
+        };
         /*private static String[] rodzZenskiGr2 = {
             "c", "cz", "dz", "rz", "sz", "z" //dż,ż zamiast z
             };*/
@@ -51,66 +54,99 @@ namespace FileScanner.Preprocessing
                                                              {"kem", "kiem"},
                                                              {"ky", "ki"},
                                                              {"ge", "dze"}
+                                                             //uzupełnić
         };
 
         //String phrase;
         //znajduje tylko odmiany rzeczowników nieżywotnych
         //jeszcze nie działa poprawnie
-        //tylko liczba pojedyncza
+        //na razie tylko liczba pojedyncza
+        //zwraca określoną formę tylko jeden raz
         public IEnumerable<String> GetVariations(String phrase)
         {
-            //rozpoznajRodzaj(String phrase);
-            //String phrase = _phrase;
-            IEnumerable<String> result;
-            if (phrase.EndsWith("a"))
+            String[] phrases = phrase.Split(' ');
+                IEnumerable<String>[] results = new IEnumerable<String>[phrases.Length];
+                for (int i=0;i<results.Length;++i) 
+                {
+                    String word = phrases[i];
+                    results[i] = GetWordVariations(word);
+                }
+                return CartesianProduct(results, 0);    
+        }
+
+        //return cartesian product of strings separated with spaces
+        private IEnumerable<String> CartesianProduct(IEnumerable<string>[] phrases, int i)
+        {
+             
+            if (i==(phrases.Length - 1)) {
+                        return phrases.ElementAt(i);
+            }
+            List<String> results = new List<String>();
+            foreach (String variation in phrases[i])
             {
-                result =  GetVariationsFeminine(phrase);
-                
+
+                IEnumerable<String> temporaryResults = CartesianProduct(phrases, i+1);
+                foreach (String res in temporaryResults) {
+                    results.Add(variation + " "+ res);
+                }
+            }
+            return results;
+        }
+
+        private IEnumerable<string> GetWordVariations(String word)
+        {
+            IEnumerable<String> result;
+            if (word.EndsWith("a"))
+            {
+                result = GetVariationsFeminine(word);
+
             }
             else
             {
-                result = GetVariationsMasculine(phrase);
+                if (word.EndsWith("o"))
+                {
+                    result = GetVariationsNeuter(word);
+                }
+                else
+                {
+                    result = GetVariationsMasculine(word);
+                }
             }
             return result;
+        }
+
+        private IEnumerable<string> GetVariationsNeuter(string phrase)
+        {
+            phrase = phrase.Remove(phrase.Length - 1);
+            String grupaDeklinacyjna = null;
+            grupaDeklinacyjna = FindDeclensionGroup(phrase, grupaDeklinacyjna, grupyDeklinacyjneN);
+            String koniecTematu = phrase.Substring(phrase.Length - 1);
+            String poczTematu = phrase.Remove(phrase.Length - 1);
+            HashSet<String> result = new HashSet<String>();
+            if (grupaDeklinacyjna == null)
+            {
+                grupaDeklinacyjna = "n2";
+            }
+            return AddSufixes(grupaDeklinacyjna, koniecTematu, poczTematu, result);
         }
 
         private IEnumerable<String> GetVariationsFeminine(String phrase)
         {
             phrase = phrase.Remove(phrase.Length-1);
             String grupaDeklinacyjna = null;
-            foreach (var pair in grupyDeklinacyjneZ)
-            {
-                for (int i = 0; i < pair.Value.Length; ++i)
-                {
-                    if (phrase.EndsWith(pair.Value[i]))
-                    {
-                        grupaDeklinacyjna = pair.Key;
-                        break;
-                    }
-                }
-                if (grupaDeklinacyjna != null)
-                {
-                    break;
-                }
-            }
+            grupaDeklinacyjna = FindDeclensionGroup(phrase, grupaDeklinacyjna, grupyDeklinacyjneZ);
             String koniecTematu = phrase.Substring(phrase.Length - 1);
             String poczTematu = phrase.Remove(phrase.Length - 1);
             HashSet<String> result = new HashSet<String>();
             if (grupaDeklinacyjna==null) {
                 grupaDeklinacyjna = "z2";
             }
-            for (int i = 0; i < sufixes[grupaDeklinacyjna].Length; ++i)
-            {
-                //String s = poczTematu + Obocznosci(koniecTematu+sufixFeminine[i]);
-                result.Add(poczTematu+koniecTematu+sufixes[grupaDeklinacyjna][i]);
-            }
-            return result;
+            return AddSufixes(grupaDeklinacyjna, koniecTematu, poczTematu, result);
         }
 
-    private IEnumerable<String> GetVariationsMasculine(String phrase)
+        private static string FindDeclensionGroup(String phrase, String grupaDeklinacyjna, Dictionary<string, string[]> declensionDictionary)
         {
-            String grupaDeklinacyjna = null;
-            foreach (var pair in grupyDeklinacyjneM)
+            foreach (var pair in declensionDictionary)
             {
                 for (int i = 0; i < pair.Value.Length; ++i)
                 {
@@ -125,6 +161,30 @@ namespace FileScanner.Preprocessing
                     break;
                 }
             }
+            return grupaDeklinacyjna;
+        }
+
+        private static IEnumerable<string> AddSufixes(String grupaDeklinacyjna, String koniecTematu, String poczTematu, HashSet<String> result)
+        {
+            for (int i = 0; i < sufixes[grupaDeklinacyjna].Length; ++i)
+            {
+                //String s = poczTematu + Obocznosci(koniecTematu+sufixFeminine[i]);
+                String s = koniecTematu + sufixes[grupaDeklinacyjna][i];
+                if (wymiany.ContainsKey(s))
+                {
+                    s = wymiany[s];
+                }
+                //trzeba usunąć polskie znaki
+                result.Add(poczTematu + s);
+            }
+           
+            return result;
+        }
+
+    private IEnumerable<String> GetVariationsMasculine(String phrase)
+        {
+            String grupaDeklinacyjna = null;
+            grupaDeklinacyjna = FindDeclensionGroup(phrase, grupaDeklinacyjna, grupyDeklinacyjneM);
             String koniecTematu = phrase.Substring(phrase.Length - 1);
             String poczTematu = phrase.Remove(phrase.Length - 1);
             HashSet<String> result = new HashSet<String>();
@@ -132,20 +192,34 @@ namespace FileScanner.Preprocessing
             {
                 grupaDeklinacyjna = "m2";
             }
-            for (int i = 0; i < sufixes[grupaDeklinacyjna].Length; ++i)
-            {
-                //String s = poczTematu + Obocznosci(koniecTematu+sufixFeminine[i]);
-                result.Add(poczTematu + koniecTematu + sufixes[grupaDeklinacyjna][i]);
-            }
-            return result;
+            return AddSufixes(grupaDeklinacyjna, koniecTematu, poczTematu, result);
         }
 
-        private String Obocznosci(String koniec) {
-            if (wymiany.ContainsKey(koniec)) {
-                    return wymiany[koniec];
-            } else {
-                   return koniec;
-            }
-        }
+    //from Normalizer
+    private static Dictionary<char, char> polishEnglishMapping = new Dictionary<char, char>() {
+            {'ą', 'a'},
+            {'ć', 'c'},
+            {'ę', 'e'},
+            {'ł', 'l'},
+            {'ń', 'n'},
+            {'ó', 'o'},
+            {'ś', 's'},
+            {'ź', 'z'},
+            {'ż', 'z'}
+        };
+    private String RemovePolishCharacters(String phrase)
+    {
+        string lowerCasePhrase = phrase.ToLower();
+        char[] phraseCharacters = lowerCasePhrase.ToCharArray();
+        var normalizedCharacters = phraseCharacters.Select(GetNormalizedCharacter);
+        return new String(normalizedCharacters.ToArray());
+    }
+    private char GetNormalizedCharacter(char c)
+    {
+        if (polishEnglishMapping.ContainsKey(c))
+            return polishEnglishMapping[c];
+        else
+            return c;
+    }
     }
 }
