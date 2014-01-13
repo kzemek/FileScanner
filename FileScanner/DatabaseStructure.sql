2add0dcaae111b9e157ccddc55446706c9a5fe97
Konrad Zemek <konrad.zemek@gmail.com>
2014-01-13 01:51:28 +0100

    Make the project publishable.

---
+++ FileScanner/DatabaseStructure.sql
@@ -0,0 +1,33 @@
+-- Table: searches
+CREATE TABLE searches ( 
+    search_id           INTEGER  PRIMARY KEY AUTOINCREMENT,
+    startTime           BIGINT,
+    endTime             BIGINT,
+    processedFilesCount INTEGER 
+);
+
+-- Table: files
+CREATE TABLE files ( 
+    file_id     INTEGER         PRIMARY KEY AUTOINCREMENT,
+    fileName    VARCHAR( 256 ),
+    fullPath    VARCHAR( 256 ),
+    sizeInBytes INTEGER,
+    search_id                   NOT NULL
+                                REFERENCES searches ( search_id ) ON DELETE CASCADE
+                                                                  ON UPDATE CASCADE 
+);
+
+-- Table: matches
+CREATE TABLE matches ( 
+    file_id  INTEGER         REFERENCES files ( file_id ),
+    [index]  INTEGER,
+    value    VARCHAR( 256 ),
+    match_id INTEGER         PRIMARY KEY AUTOINCREMENT 
+);
+
+-- Table: phrases
+CREATE TABLE phrases ( 
+    search_id INTEGER         REFERENCES searches ( search_id ),
+    phrase_id INTEGER         PRIMARY KEY AUTOINCREMENT,
+    phrase    VARCHAR( 256 ) 
+);
